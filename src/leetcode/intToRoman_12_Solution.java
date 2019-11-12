b9af493dfbccf60e2759da843bb2f66abe91b396
HusterHaoYoung <younghaodaydayupup@163.com>
2019-11-12 17:10:37 +0800

    上传12题

---
+++ src/leetcode/intToRoman_12_Solution.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+public class intToRoman_12_Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num >= 1000) {
+            sb.append("M");
+            num -= 1000;
+        }
+        if (num >= 900) {
+            sb.append("CM");
+            num -= 900;
+        }
+        if (num >= 500) {
+            sb.append("D");
+            num -= 500;
+        }
+        if (num >= 400) {
+            sb.append("CD");
+            num -= 400;
+        }
+        while (num >= 100) {
+            sb.append("C");
+            num -= 100;
+        }
+        if (num >= 90) {
+            sb.append("XC");
+            num -= 90;
+        }
+        if (num >= 50) {
+            sb.append("L");
+            num -= 50;
+        }
+        if (num >= 40) {
+            sb.append("XL");
+            num -= 40;
+        }
+        while (num >= 10) {
+            sb.append("X");
+            num -= 10;
+        }
+        if (num >= 9) {
+            sb.append("IX");
+            num -= 9;
+        }
+        if (num >= 5) {
+            sb.append("V");
+            num -= 5;
+        }
+        if (num >= 4) {
+            sb.append("IV");
+            num -= 4;
+        }
+        while (num >= 1) {
+            sb.append("I");
+            num--;
+        }
+        return sb.toString();
+    }
+
+    public String intToRoman1(int num) {
+        StringBuilder sb = new StringBuilder();
+        String[] strings = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int index = 0;
+        while (num > 0) {
+            while (num >= nums[index]) {
+                sb.append(strings[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return sb.toString();
+    }
+}
